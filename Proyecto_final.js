26acd29493e33bcb24949b5277d4b5669dd4fc7d
CrisSalas23 <126738481+CrisSalas23@users.noreply.github.com>
2023-03-30 23:12:07 -0300

    Add files via upload

---
+++ Proyecto_final.js
@@ -0,0 +1,16 @@
+/**intenté minimizar codigo para no repetir tanto pero a la hora de presentar no me salia como yo queria */
+function mostrar(){
+    document.getElementById('opción1').style.display = 'block'; 
+}
+
+function mostrar2(){
+    document.getElementById('opción2').style.display = 'block'; 
+}
+
+function mostrar3(){
+    document.getElementById('opción3').style.display = 'block'; 
+}
+
+function mostrar4(){
+    document.getElementById('opción4').style.display = 'block'; 
+}
